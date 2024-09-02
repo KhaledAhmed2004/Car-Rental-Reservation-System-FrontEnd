4ed063a2ee2cdc74a801f66be3847e98c41ef4c0
KhaledAhmed2004 <khaledahmednayeem2004@gmail.com>
2024-09-02 22:30:22 +0600

    feat(about-us): add contact us section

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -2,12 +2,16 @@ import React from "react";
 import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../layouts/MainLayout";
 import HomePage from "../pages/HomePage";
+import AboutUsPage from "../pages/AboutUsPage";
 
 const Router = createBrowserRouter([
   {
     path: "/",
     element: <MainLayout />,
-    children: [{ path: "/", element: <HomePage /> }],
+    children: [
+      { path: "/", element: <HomePage /> },
+      { path: "/aboutUs", element: <AboutUsPage /> },
+    ],
   },
 ]);
 export default Router;
